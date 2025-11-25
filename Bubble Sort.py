c206de6d86f35948a3d642ab9f92cab804889ea4
Isaac.Fairno <i.fairnington09@gmail.com>
2025-11-25 13:23:38 +0000

    Add files via upload

---
+++ Bubble Sort.py
@@ -0,0 +1,36 @@
+# 1 enter a list of data items
+import random,time
+
+
+
+y = int(input("Enter a length of an array"))
+items = [random.randint(0,100) for x in range (y)]
+start = time.time()
+
+print(f"Original List of {y} numbers: {items}")
+n = len(items)-1
+
+swap = True
+
+while (swap) and n>0: # While loop to only iterate through the array ONLY if swaps are made (swap = True)
+    swap = False # Swap is False to break the loop
+    
+    for x in range(0,n): # For loop to iterate through the array
+        
+        if items[x]>items[x+1]: # If the current value is greater than the next value the swap the two values
+
+            temp = items[x]
+            items[x] = items[x+1]
+            items[x+1] = temp
+     
+            swap = True # Sets swap to True so it loops round again
+    n = n-1 #Shortens the list by 1 each pass
+end = time.time()
+speed = round(end - start, 6) #Makes the time precise
+
+print(items)
+print(f"Time Taken: {speed}")
+
+
+ 
+        
